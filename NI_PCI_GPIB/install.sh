a44d01b61dbdc52ecb133d7ff066ce17f1df2a61
Nikolai Nekrutenko <nekrutnikolai@gmail.com>
2022-07-12 19:46:06 +0200

    updated the installation script

--- NI_PCI_GPIB/install.sh
+++ NI_PCI_GPIB/install.sh
@@ -1,36 +1,28 @@
 #!/bin/bash
-
-# Installation script for Linux GPIB
+# installation script for Linux GPIB
+# created by nan34 with inspiration from https://arakiliu.github.io/2020/12/22/raspi-usb-gpib/
 
 sudo apt install -y tk-dev build-essential texinfo texi2html libcwidget-dev libncurses5-dev libx11-dev binutils-dev bison flex libusb-1.0-0 libusb-dev libmpfr-dev libexpat1-dev tofrodos subversion autoconf automake libtool python3-dev python3-pip
 
-cd ~
+# make a directory for the drivers 
 
-sudo mkdir linux-gpib 
+sudo mkdir ~/linux-gpib 
 
-cd linux-gpib/
+cd ~/linux-gpib/
 
 sudo svn checkout svn://svn.code.sf.net/p/linux-gpib/code/trunk linux-gpib-code
 
-cd linux-gpib-kernel
+cd ~/linux-gpib/linux-gpib-code/linux-gpib-kernel
 
-sudo make
+# install the packages at the kernel level
 
-sudo make install
+sudo make && sudo make install
 
-## check that the directories do indeed work!
+## install the packages at the user level
 
-cd ~/linux-gpib/linux-gpib-user
+cd ~/linux-gpib/linux-gpib-code/linux-gpib-user
 
-sudo ./bootstrap
-
-sudo ./configure
-
-sudo make
-
-sudo make install
-
-sudo find / -name gpib.conf
+sudo ./bootstrap && sudo ./configure && sudo make && sudo make install
 
 sudo nano /usr/local/etc/gpib.conf
 
